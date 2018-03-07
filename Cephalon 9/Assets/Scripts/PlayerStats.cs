f914ebf22a9d0cac2aed51087017f49799a47402
Xenolane <30466183+Xenolane@users.noreply.github.com>
2018-03-07 18:02:08 +0000

    Cephalon 9
    Platformer Assests

---
+++ Cephalon 9/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    // Singleton
+    private static PlayerStats _instance;
+    public static PlayerStats Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<PlayerStats>();
+            }
+
+            return _instance;
+        }
+    }
+
+
+
+}
---
+++ Cephalon 9/Library/Collab/Base/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool dirLeft;
+
+    private Transform enemy;
+
+    private Rigidbody2D r2D;
+
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
