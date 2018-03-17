1ee61cc915416f67f0dead6908bc3079442fe195
Rik Roy <rikr496@gmail.com>
2018-03-17 16:02:06 +0530

    moving Blocks
    customizable moving blocks added

---
+++ Assets/Scripts/MovingBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour {
+
+	[SerializeField]private Vector3 motion;
+
+	void Update () {
+		transform.Translate (motion*Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.CompareTag ("boundBlock")) {
+			this.motion = other.GetComponent <boundBlock> ().motion;
+		}
+	}
+
+}
---
+++ Assets/Scripts/boundBlock.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boundBlock : MonoBehaviour {
+
+	public Vector3 motion;
+
+}
