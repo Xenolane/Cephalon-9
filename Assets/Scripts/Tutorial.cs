3b7fa28c5ed2330ceb6b10ee6ed0ecb0d463d2c0
Xenolane <30466183+Xenolane@users.noreply.github.com>
2018-06-14 23:36:10 +0100

    Tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour {
+
+    public GameObject objectivesUI1;
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            StartCoroutine(TutorialUI());
+        }
+    }
+
+    IEnumerator TutorialUI()
+    {
+        objectivesUI1.SetActive(true);
+        yield return new WaitForSeconds(10);
+        objectivesUI1.SetActive(false);
+        Destroy(this.gameObject);
+    }
+}
